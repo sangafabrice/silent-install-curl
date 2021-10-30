fddaf1e49f706b700e10a3d1904f5b62aec16440
Fabrice Sanga <69244030+sangafabrice@users.noreply.github.com>
2021-10-31 00:31:26 +0200

    --amend

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,3 @@
-Set-Location -Path $MyInvocation.MyCommand.Path
+Set-Location -Path ($MyInvocation.MyCommand.Path -replace '\\[^\\]+$')
 Import-Module .\CurlUpdater.psm1
 Update-Curl -Save H:\Software\cURL
